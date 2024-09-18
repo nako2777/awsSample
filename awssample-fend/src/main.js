689c15478864d4587d6c646ea5c56c5549b972b9
takuu <takuu.tei@sun-star.co.jp>
2024-09-18 21:55:14 +0900

    update

--- awssample-fend/src/main.js
+++ awssample-fend/src/main.js
@@ -3,7 +3,8 @@ import './assets/main.css'
 import { Quasar } from 'quasar'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
 import App from './App.vue'
 import router from './router'
 // Import icon libraries
@@ -12,6 +13,8 @@ import '@quasar/extras/material-icons/material-icons.css'
 // Import Quasar css
 import 'quasar/src/css/index.sass'
 const app = createApp(App)
+Amplify.configure(awsconfig);
+
 
 app.use(Quasar,{
     plugins:{}
--- awssample-fend/src/views/CognitoPage.vue
+++ awssample-fend/src/views/CognitoPage.vue
@@ -1,31 +1,71 @@
 <template>
-    <div>
-        <!-- 登録ボックス -->
-        <div class="boxWrapper">
-            <p class="pCenter">登録</p>
-            <q-input dense rounded outlined bg-color="white" v-model="signupUserName" type="text" label="ユーザネーム" />
-            <br>
-            <q-input dense rounded outlined bg-color="white" v-model="signuppassword" type="text" label="パスワード" />
-            <br>
-            <div class="keepCenter">
-                <q-btn rounded class="btn" color="var(--vt-c-black-soft)" dense label="登録" @click="onClick" />
-            </div>
-        </div>
-        <!-- ログインボックス -->
+  <div>
+    <!-- 登録ボックス -->
+    <div class="boxWrapper">
+      <p class="pCenter">登録</p>
+      <q-input
+        dense
+        rounded
+        outlined
+        bg-color="white"
+        v-model="signupUserName"
+        type="text"
+        label="ユーザネーム"
+      />
+      <br />
+      <q-input
+        dense
+        rounded
+        outlined
+        bg-color="white"
+        v-model="signupPassword"
+        type="text"
+        label="パスワード"
+      />
+      <br />
+      <div class="keepCenter">
+        <q-btn
+          rounded
+          class="btn"
+          color="var(--vt-c-black-soft)"
+          dense
+          label="登録"
+          @click="signup"
+        />
+      </div>
     </div>
+    <!-- ログインボックス -->
+  </div>
 </template>
 
+<script setup>
+import { ref } from 'vue'
+import { Auth } from 'aws-amplify';
+const signupUserName = ref()
+const signupPassword = ref()
+const signup = async () => {
+  console.log(signupUserName.value)
+  console.log(signupPassword.value)
+
+  const result = await Auth.signup({
+    username: signupUserName.value,
+    password: signupPassword.value
+  })
+
+  console.log(result)
+}
+</script>
+
 <style scoped>
-    .boxWrapper {
-        padding: 20px;
-        
-    }
+.boxWrapper {
+  padding: 20px;
+}
 
-    .pCenter{
-        text-align: center;
-    }
+.pCenter {
+  text-align: center;
+}
 
-    .btn{
-        color:  var(--vt-c-text-dark-2) !important;
-    }
+.btn {
+  color: var(--vt-c-text-dark-2) !important;
+}
 </style>
